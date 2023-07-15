cefa489b77af59f97934383932bcda72d05bfbca
nbaciyarkin <kbyarkin@gmail.com>
2023-07-15 16:08:31 +0300

    Initial Commit

---
+++ HUBX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HUBX
+//
+//  Created by Yarkın Gazibaba on 15.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
